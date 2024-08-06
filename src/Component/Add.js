57d9232f4336fdf9b915928242312431c1cbf3f6
anshuljuet21 <anshul.juet21@gmail.com>
2024-08-06 09:56:59 +0530

    third commit

--- src/Component/Add.js
+++ src/Component/Add.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react'
 
 export class Add extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         first_name:'', last_name:'',email:''
+      }
+    }
+
+    changeHandler=(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+    submitHandler=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+    }
+    
   render() {
     return (
       <div>
         <div className='alert alert-success'>Add User</div>
-        <form className='container'>
+        <form className='container' onSubmit={this.submitHandler}>
             <div className='row my-2'>
                 <div className='col'>
-                    <input type="text" name="first_name" placeholder="Enter First Name" className="form-control"/>
+                    <input type="text" name="first_name" placeholder="Enter First Name" className="form-control" defaultValue={this.state.first_name} onChange={this.changeHandler}/>
                 </div>
-                <div classname='col'>
-                <input type="text" name="Last_name" placeholder="Enter Last Name" className="form-control"/>
+                <div className='col'>
+                <input type="text" name="last_name" placeholder="Enter Last Name" className="form-control" defaultValue={this.state.last_name} onChange={this.changeHandler}/>
                 </div>
              </div>
              <div className='row my-3'>
                 <div className='row my-3'>
                     <div className='col'>
-                        <input type="text" name="email" placeholder='Enter Email' className='form-control'/>
+                        <input type="text" name="email" placeholder='Enter Email' className='form-control' defaultValue={this.state.email} onChange={this.changeHandler}/>
                     </div>
 
                 </div>
